637a140d3c76759100fb300bea5851b76cd6beed
nahian28 <nahiansylhet@gmail.com>
2026-01-15 23:23:06 +0600

    Init

--- inc/orders/afd-orders-print-kitchen.php
+++ inc/orders/afd-orders-print-kitchen.php
@@ -34,7 +34,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'print' && $_GET['type'] === '
     width: 100%;
 }
             .afd-print {
-                width: 80mm;
+                width: 90mm;
                 margin: 0;
                 padding: 2mm; /* Internal padding for text safety */
                 box-sizing: border-box;
